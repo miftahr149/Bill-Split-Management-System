2fbd6e7c0cd8c949adbd3c68cf8bc2ce949484c4
miftahr149 <miftahr149@gmail.com>
2024-05-04 13:13:26 +0800

    Change the Database operation
    - Adding requested bill split database
    - tweak around Database class

--- lib/storage.h
+++ lib/storage.h
@@ -8,22 +8,132 @@
 #include <fstream>
 #include <string>
 #include <cmath>
+#include <type_traits>
 
 namespace storage
 {
-  struct User
+  class MasterClass
+  {
+  public:
+    virtual void extract(std::vector<std::string>) {}
+    virtual void save(std::ofstream &) {}
+  };
+
+  class BillSplit : MasterClass
+  {
+    std::string name;
+    int debt;
+    int numDebt;
+    std::vector<std::string> nameDebt;
+    int numPaymentSuccessfull;
+    std::vector<std::string> namePaymentSuccessful;
+
+  public:
+    BillSplit() {}
+    BillSplit(std::vector<std::string> array) { this->extract(array); }
+
+    void extract(std::vector<std::string> array)
+    {
+      int counter = 0;
+      int nextDestination = 2;
+      int counterCycle = 0;
+
+      for (int i = 0; i < array.size(); i++)
+      {
+        if (i == 0)
+        {
+          this->name = array[i];
+          continue;
+        }
+
+        if (i == 1)
+        {
+          this->debt = std::stoi(array[i]);
+          continue;
+        }
+
+        if (i == nextDestination)
+        {
+          counter = std::stoi(array[i]);
+          nextDestination += counter + 1;
+          if (counterCycle == 0)
+            this->numDebt = counter;
+          else
+            this->numPaymentSuccessfull = counter;
+          counterCycle++;
+          continue;
+        }
+
+        if (counterCycle == 1)
+          this->nameDebt.push_back(array[i]);
+        else
+          this->namePaymentSuccessful.push_back(array[i]);
+        counter--;
+      }
+    }
+
+    void save(std::ofstream &output)
+    {
+      output << this->name << ",";
+      output << this->debt << ",";
+      output << this->numDebt << ",";
+
+      for (auto peopleName : this->nameDebt)
+        output << peopleName << ",";
+
+      output << this->numPaymentSuccessfull << ",";
+
+      for (auto peopleName : this->namePaymentSuccessful)
+      {
+        output << peopleName;
+        if (peopleName == this->namePaymentSuccessful.back())
+          output << std::endl;
+        else
+          output << ",";
+      }
+    }
+
+    BillSplit(const BillSplit &obj1)
+    {
+      this->name = obj1.name;
+      this->debt = obj1.debt;
+      this->numDebt = obj1.numDebt;
+      this->nameDebt = obj1.nameDebt;
+      this->numPaymentSuccessfull = obj1.numPaymentSuccessfull;
+      this->namePaymentSuccessful = obj1.namePaymentSuccessful;
+    }
+  };
+
+  class User : MasterClass
   {
     std::string name;
     std::string password;
     std::string type;
-  };
 
-  struct BillSplit
-  {
-  };
+  public:
+    User() {}
+    User(std::vector<std::string> array) { this->extract(array); }
 
-  class RequestedBillSplit
-  {
+    User(const User &obj)
+    {
+      this->name = obj.name;
+      this->password = obj.password;
+      this->type = obj.type;
+    }
+
+    void extract(std::vector<std::string> array) override
+    {
+      this->name = array[0];
+      this->password = array[1];
+      this->type = array[2];
+    }
+
+    void save(std::ofstream &output) override
+    {
+      output << name << ',';
+      output << password << ',';
+      output << type << std::endl;
+    }
   };
 
   std::vector<int> findSubStrIndex(std::string str, char sep)
@@ -54,35 +164,29 @@ namespace storage
     int highestId;
     std::string dirname;
     std::map<int, T> data;
-    std::function<void(std::ofstream&, int, T)> saveFunc;
+    std::function<void(std::ofstream &, int, T)> saveFunc;
 
   public:
-    Database(
-        std::string dirname,
-        std::function<void(T &, int, std::string)> extractFunc,
-        std::function<void(std::ofstream&, int, T)> saveFunc)
-        : dirname(dirname), highestId(0)
+    Database(std::string dirname) : dirname(dirname), highestId(0)
     {
       std::ifstream inputStream(this->dirname);
 
       while (!inputStream.eof())
       {
         std::string inputResult;
-        std::getline(inputStream, inputResult); 
-        
-        if (inputResult == "") break;
+        std::getline(inputStream, inputResult);
+
+        if (inputResult == "")
+          break;
 
         auto extract = split(inputResult);
         int id = std::stoi(extract[0]);
+        extract.erase(extract.begin());
 
         if (std::max(id, this->highestId) == id)
           this->highestId = id;
 
-        T newData;
-        for (int i = 1; i < extract.size(); i++)
-          extractFunc(newData, i, extract[i]);
-
-        this->add(newData, id);
+        this->add(T(extract), id);
       }
 
       inputStream.close();
@@ -95,13 +199,16 @@ namespace storage
       this->data[id] = newData;
     }
 
-    void update(int id, T newData){this->data[id] = newData;}
+    void update(int id, T newData) { this->data[id] = newData; }
 
     void save()
     {
       std::ofstream outputStream(this->dirname);
-      for (const auto element : data)
-        this->saveFunc(outputStream, element.first, element.second);
+      for (auto element : data)
+      {
+        outputStream << element.first << ",";
+        element.second.save(outputStream);
+      }
       outputStream.close();
     }
 
--- main.cpp
+++ main.cpp
@@ -1,39 +1,17 @@
 #include <iostream>
 #include "utility.h"
 #include "storage.h"
+
 int main()
 {
   if (util::isFolderEmpty("storage"))
     util::initFolder();
 
-  storage::Database<storage::User> userData(
-      "storage/user.txt",
-      [](storage::User &user, int i, std::string str)
-      {
-        if (i == 1)
-          user.name = str;
-        if (i == 2)
-          user.password = str;
-        if (i = 3)
-          user.type = str;
-      },
-      [](std::ofstream &outputStream, int id, storage::User user)
-      {
-        outputStream << id << ",";
-        outputStream << user.name << ",";
-        outputStream << user.password << ",";
-        outputStream << user.type;
-        outputStream << std::endl;
-      });
-
-  storage::Database<storage::BillSplit> billSplitData(
-      "storage/billSplit.txt",
-      [](storage::BillSplit &billSplit, int id, std::string str) {
-        
-      },
-      [](std::ofstream &outputStream, int id, storage::BillSplit billSplit) {
-
-      });
+  storage::Database<storage::User> userData("storage/user.txt");
+  storage::Database<storage::BillSplit> billSplitData("storage/billSplit.txt");
+  
+  std::string str = "storage/requestedBillSplit"; 
+  storage::Database<storage::BillSplit> requestedBillSplit(str);
 
   return 0;
 }
